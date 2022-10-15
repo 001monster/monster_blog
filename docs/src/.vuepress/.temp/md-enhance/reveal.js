3a44f119ccc650e41fa9f9cfee0f04bf5c4918db
zhangshuwei <2966211270@qq.com>
2022-10-15 15:11:23 +0800

    !

--- docs/src/.vuepress/.temp/md-enhance/reveal.js
+++
@@ -1,4 +0,0 @@
-import { reveal, revealMarkdown, revealHighlight, revealMath, revealSearch, revealNotes, revealZoom } from "D:/桌面/个人博客/docs/node_modules/vuepress-plugin-md-enhance/lib/client/reveal/index.js";
-
-export const useReveal = () => [reveal(), revealMarkdown(), revealHighlight(), revealMath(), revealSearch(), revealNotes(), revealZoom()
-];
