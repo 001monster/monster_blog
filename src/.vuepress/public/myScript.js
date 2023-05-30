d0b18655f8d5c71db2f0298e1305156519dc1d1a
ZSW <2966211270@qq.com>
2023-05-30 18:00:35 +0800

    ！

--- src/.vuepress/public/myScript.js
+++ src/.vuepress/public/myScript.js
@@ -1,9 +1,13 @@
 
-let bodyDom = document.getElementsByTagName('body')[0]
-bodyDom.style.backgroundImage = `url(/assets/images/cover${Math.round(Math.random() * 9)}.jpg)`
-let i = 1
-setInterval(() => {
-    if (i > 9) i = 1
-    bodyDom.style.backgroundImage = `url(/assets/images/cover${i}.jpg)`
-    i++
-}, 20000);
+
+
+window.onload = function() {
+    let bodyDom = document.getElementsByTagName('body')[0]
+    bodyDom.style.backgroundImage = `url(/assets/images/cover${Math.round(Math.random() * 9)}.jpg)`
+    let i = 1
+    setInterval(() => {
+        if (i > 9) i = 1
+        bodyDom.style.backgroundImage = `url(/assets/images/cover${i}.jpg)`
+        i++
+    }, 20000);
+}
