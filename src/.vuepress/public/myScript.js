f9548b11b378e98665de42afa5535d9bd7dc90c3
ZSW <2966211270@qq.com>
2023-05-30 17:52:30 +0800

    ！

--- src/.vuepress/public/myScript.js
+++ src/.vuepress/public/myScript.js
@@ -1,9 +1,9 @@
 
-let body = document.getElementsByTagName('body')[0]
-body.style.backgroundImage = `url(/assets/images/cover${Math.round(Math.random() * 9)}.jpg)`
+let bodyDom = document.getElementsByTagName('body')[0]
+bodyDom.style.backgroundImage = `url(/assets/images/cover${Math.round(Math.random() * 9)}.jpg)`
 let i = 1
 setInterval(() => {
     if (i > 9) i = 1
-    body.style.backgroundImage = `url(/assets/images/cover${i}.jpg)`
+    bodyDom.style.backgroundImage = `url(/assets/images/cover${i}.jpg)`
     i++
 }, 20000);
